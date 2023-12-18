9d87788e79b67aa6e89ae82be90c17990b3d7bcc
carlosavellar <carlos.avellar@gmail.com>
2023-12-18 12:48:59 -0300

    File System

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,27 @@
+import fs from "fs";
+import path from "path";
+
 import { Fragment } from "react";
 function HomePage(props) {
-  console.log(props.products);
   return (
     <Fragment>
       <ul>
         {props.products.map((item) => (
           <li key={item.id}>{item.title}</li>
         ))}
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
       </ul>
     </Fragment>
   );
 }
 
 export async function getStaticProps() {
+  debugger;
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
-    props: {
-      products: [{ id: "p1", title: "Product 1" }],
-    },
+    props: { products: data.products },
   };
 }
 
