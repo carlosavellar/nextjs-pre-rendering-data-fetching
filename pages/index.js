a7ccc1655e7e1ae605ba19dbd71ce2d497fdcf3c
carlosavellar <carlos.avellar@gmail.com>
2023-12-20 19:49:22 -0300

    Using file system

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
 import { Fragment } from "react";
+import path from "path";
+const fs = typeof window === "undefined" ? require("fs/promises") : null;
+
 function HomePage(props) {
   const { products } = props;
   return (
     <Fragment>
-      {props.products.map((product) => (
-        <li key={product.id}>{product.title}</li>
-      ))}
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {props.products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
       </ul>
     </Fragment>
   );
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: "p1", title: "Product1" }],
+      products: data.products,
     },
   };
 }
